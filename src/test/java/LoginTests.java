5765c98246c77f2fe2fadd56405c9defa8a035b0
Percy <pcyrodm3@gmail.com>
2019-02-01 22:19:36 -0500

    commit 4

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -4,6 +4,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
@@ -31,6 +32,7 @@ public class LoginTests {
         Assert.assertEquals(driver.getTitle(), "Web Orders");
     }
 
+
     @Test
     public void logOutTest() {
         driver.get("http://secure.smartbearsoftware.com/samples/testcomplete12/WebOrders/login.aspx");
@@ -39,6 +41,16 @@ public class LoginTests {
         //Assert.assertEquals(driver.getTitle(), "Web Orders");
         driver.findElement(By.id("ctl00_logout")).click();
         Assert.assertEquals(driver.getTitle(), "Web Orders Login");
+    }
+
+    @Test
+    public void negativeloginTest() {
+        driver.get("http://secure.smartbearsoftware.com/samples/testcomplete12/WebOrders/login.aspx");
+        driver.findElement(By.id("ctl00_MainContent_username")).sendKeys("Tester2");
+        driver.findElement(By.id("ctl00_MainContent_password")).sendKeys("test2" + Keys.ENTER);
+        String errorMsg = driver.findElement(By.id("ctl00_MainContent_status")).getText();
+
+        Assert.assertEquals(errorMsg, "Invalid Login or Password.");
     }
 
 
